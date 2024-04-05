2d967d40c39548aad7e2f7e8cb6830861d762405
annisasha <152659249+annisasha@users.noreply.github.com>
2024-04-05 17:24:47 +0700

    edit2

--- assessment1-pabw.js
+++ assessment1-pabw.js
@@ -6,14 +6,14 @@ let buku = [
   
   function cari() {
     let jumlahInput = parseInt(document.getElementById("jumlah").value);
-    let buku = document.getElementById("buku").value;
+    let bukuDipilih = document.getElementById("buku").value;
   
     if (jumlahInput === "" || isNaN(jumlahInput) || jumlahInput <= 0) {
       alert("Jumlah tidak valid atau kosong.");
     } else {
       let found = false;
       for (let i = 0; i < buku.length; i++) {
-        if (buku[i].nama === buku) {
+        if (buku[i].nama === bukuDipilih) {
           found = true;
           if (buku[i].jumlah < jumlahInput) {
             alert("Jumlah buku tidak tersedia.");
